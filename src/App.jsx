0b136d310c8c63864e9363618dece9121eee7e39
Josue Pimentel <Josuedavidpimentel05@gmail.com>
2024-10-18 11:41:43 -0400

    Actualizacion
    Se agrego una nueva animacion para el fondo

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,18 @@
 import { GlobalProvider } from "./Context/GlobalState";
-import Header from "./Components/Header";
+// import Header from "./Components/Header";
 import Balance from "./Components/Balance";
 import TransactionForm from "./Components/Transactions/TransactionForm";
 import TransactionList from "./Components/Transactions/TransactionList";
 import IncomeExpenses from "./Components/IncomeExpenses";
 import ExpenseChart from "./Components/ExpenseChart";
+
 function App() {
   return (
     <GlobalProvider>
-      <div className="bg-[#161719] text-white h-screen flex justify-center items-center">
+      <div className=" text-white h-screen flex justify-center items-center">
         <div className="bg-[#1f1e1e] p-10 rounded-lg flex gap-x-2 border-2 border-neutral-800  opacity-80">
           <div className="container mx-auto w-3/6">
-            <h1 className=" text-4xl font-bold">Seguidor de Gastos</h1> 
+            <h1 className=" text-4xl font-bold">Seguidor de Gastos</h1>
             <IncomeExpenses />
             <Balance />
             <TransactionForm />
@@ -22,6 +23,7 @@ function App() {
           </div>
         </div>
       </div>
+      <div className="w-[200vw] h-[200vw] bg-gradient-to-tr from-slate-800 to-zinc-900 absolute top-[-50vw] right-[-50vw] overflow-hidden -z-30 animate-spin duration-700 "></div>
     </GlobalProvider>
   );
 }
