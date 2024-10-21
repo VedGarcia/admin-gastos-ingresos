3a5350bfb9a913c46460a83a436894e27886d602
Samucker <zsamuel513@gmail.com>
2024-10-20 22:07:12 -0400

    Añadir boton expense

--- src/components/transactions/TransactionForm.jsx
+++ src/components/transactions/TransactionForm.jsx
@@ -5,23 +5,38 @@ export function TransactionForm() {
   const { addTransaction } = useGlobalState();
 
   const [description, setDescription] = useState("");
-  const [amount, setAmount] = useState(0);
+  const [income, setIncome] = useState(0);  
+  const [expense, setExpense] = useState(0);  
 
   const onSubmit = (e) => {
     e.preventDefault();
-    addTransaction({
-      id: window.crypto.randomUUID(),
-      description,
-      amount: +amount,
-    });
+
+    if (income) {
+      addTransaction({
+        id: window.crypto.randomUUID(),
+        description,
+        amount: +income,  
+      });
+    }
+
+    if (expense) {
+      addTransaction({
+        id: window.crypto.randomUUID(),
+        description,
+        amount: -Math.abs(expense),  
+      });
+    }
+
 
     setDescription("");
-    setAmount(0);
+    setIncome(0);
+    setExpense(0);
   };
 
   return (
     <div className="p-6 bg-white bg-opacity-80 backdrop-blur-lg rounded-md shadow-lg animate-reappear">
       <form onSubmit={onSubmit}>
+
         <input
           type="text"
           onChange={(e) => setDescription(e.target.value)}
@@ -29,17 +44,29 @@ export function TransactionForm() {
           className="bg-gray-300 text-gray-800 px-4 py-2 rounded-md block mb-4 w-full focus:outline-none focus:ring-2 focus:ring-blue-400 transition"
           value={description}
         />
+
+
         <input
           type="number"
-          onChange={(e) => setAmount(e.target.value)}
+          onChange={(e) => setIncome(e.target.value)}
           step="0.01"
-          placeholder="0.00"
-          className="bg-gray-300 text-gray-800 px-4 py-2 rounded-md block mb-4 w-full focus:outline-none focus:ring-2 focus:ring-blue-400 transition"
-          value={amount}
+          placeholder="Income Amount"
+          className="bg-green-300 text-gray-800 px-4 py-2 rounded-md block mb-4 w-full focus:outline-none focus:ring-2 focus:ring-green-400 transition"
+          value={income}
         />
+
+        <input
+          type="number"
+          onChange={(e) => setExpense(e.target.value)}
+          step="0.01"
+          placeholder="Expense Amount"
+          className="bg-red-300 text-gray-800 px-4 py-2 rounded-md block mb-4 w-full focus:outline-none focus:ring-2 focus:ring-red-400 transition"
+          value={expense}
+        />
+
         <button
           className="bg-gradient-to-r from-green-400 to-blue-500 text-white px-4 py-2 rounded-md block w-full font-semibold transition transform hover:scale-105"
-          disabled={!description || !amount}
+          disabled={!description || (!income && !expense)}
         >
           Add Transaction
         </button>
