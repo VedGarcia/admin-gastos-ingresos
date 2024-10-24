56665c994e80615aaf4feb8d6ec1fc95b4b46e63
Josue Pimentel <Josuedavidpimentel05@gmail.com>
2024-10-24 10:09:45 -0400

    se completo la transicion
    se añadio la funcionalidad de que cuando presione el boton de login lo lleve a la aplicacion completa

---
+++ src/Components/IngresoApp/Ingreso.jsx
@@ -0,0 +1,31 @@
+
+import React from 'react';
+import Balance from "../Balance";
+import TransactionForm from "../Transactions/TransactionForm";
+import TransactionList from "../Transactions/TransactionList";
+import IncomeExpenses from "../IncomeExpenses";
+import ExpenseChart from "../ExpenseChart";
+import { GiBandit } from "react-icons/gi";
+
+function Ingreso() {
+  return (
+    <div className="bg-[#1b1c20] p-10 rounded-lg flex gap-x-2 border-2 border-neutral-800 opacity-80">
+      <div className="container mx-auto w-3/6">
+        <h1 className="text-4xl font-bold border-b py-3">
+          Balance Personal
+        </h1>
+        <IncomeExpenses />
+        <Balance />
+        <TransactionForm />
+      </div>
+      <GiBandit className="h-10 w-10" />
+      <div className="flex flex-col flex-1">
+        <ExpenseChart />
+        <TransactionList />
+      </div>
+      <div className="w-[200vw] h-[200vw] bg-gradient-to-tr from-[#171616] to-[#000000] absolute top-[-50vw] right-[-50vw] overflow-hidden -z-30"></div>
+    </div>
+  );
+}
+
+export default Ingreso;
--- src/Components/LoginForm/LoginForm.jsx
+++ src/Components/LoginForm/LoginForm.jsx
@@ -1,44 +1,51 @@
+
+import React from 'react';
 import { RiUserLine } from "react-icons/ri";
 import { TfiLock } from "react-icons/tfi";
 
-function LoginForm() {
-  return (
-    <div className="flex items-center justify-center h-screen bg-[#171616]">
-      <form className="bg-[#1b1c20] w-11/12 sm:w-3/4 md:w-1/2 lg:w-1/3 text-white p-10 rounded-lg shadow-lg">
-        <h1 className="text-3xl p-10 text-center">Acceder</h1>
-        <div className="relative mb-4 w-full">
-          <input
-            className="bg-gradient-to-r from-gray-800 to-zinc-700 text-white px-4 py-2 rounded-lg shadow-md mb-3 w-full transition duration-300 ease-in-out focus:outline-none focus:ring-2 focus:ring-blue-600"
-            type="text"
-            placeholder="Usuario"
-            required
-          />
-          <RiUserLine className="absolute right-1 top-2.5 h-6 w-6 text-gray-400" />
-        </div>
-        <div className="relative mb-4 w-full">
-          <input
-            className="bg-gradient-to-r from-gray-800 to-zinc-700 text-white px-4 py-2 rounded-lg shadow-md mb-3 w-full transition duration-300 ease-in-out focus:outline-none focus:ring-2 focus:ring-blue-600"
-            type="password"
-            placeholder="Contraseña"
-            required
-          />
-          <TfiLock className="absolute right-1 top-2.5 h-6 w-6 text-gray-400" />
-        </div>
+function LoginForm({ onLogin }) {
+  const handleSubmit = (event) => {
+    event.preventDefault();
 
-        <div className="flex justify-between items-center w-full mb-4">
-          <label className="flex items-center">
-            <input type="checkbox" className="mr-2 cursor-pointer" />Recuerdame
-          </label>
-          <a href="#" className="text-blue-500 hover:underline">¿Olvidaste la Contraseña?</a>
-        </div>
-        <button
-          className="bg-blue-600 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-lg w-full transition duration-300 ease-in-out"
-          type="submit"
-        >
-          Acceder
-        </button>
-      </form>
-    </div>
+
+    onLogin();
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className="bg-[#1b1c20] w-11/12 sm:w-3/4 md:w-1/2 lg:w-1/3 text-white p-10 rounded-lg shadow-lg">
+      <h1 className="text-3xl p-10 text-center">Acceder</h1>
+      <div className="relative mb-4 w-full">
+        <input
+          className="bg-gradient-to-r from-gray-800 to-zinc-700 text-white px-4 py-2 rounded-lg shadow-md mb-3 w-full transition duration-300 ease-in-out focus:outline-none focus:ring-2 focus:ring-blue-600"
+          type="text"
+          placeholder="Usuario"
+          required
+        />
+        <RiUserLine className="absolute right-1 top-2.5 h-6 w-6 text-gray-400" />
+      </div>
+      <div className="relative mb-4 w-full ">
+        <input
+          className="bg-gradient-to-r from-gray-800 to-zinc-700 text-white px-4 py-2 rounded-lg shadow-md mb-3 w-full transition duration-300 ease-in-out focus:outline-none focus:ring-2 focus:ring-blue-600"
+          type="password"
+          placeholder="Contraseña"
+          required
+        />
+        <TfiLock className="absolute right-1 top-2.5 h-6 w-6 text-gray-400" />
+      </div>
+
+      <div className="flex justify-between items-center w-full mb-4">
+        <label className="flex items-center">
+          <input type="checkbox" className="mr-2 cursor-pointer" />Recuerdame
+        </label>
+        <a href="#" className="text-blue-500 hover:underline">¿Olvidaste la Contraseña?</a>
+      </div>
+      <button
+        className="bg-blue-600 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-lg w-full transition duration-300 ease-in-out"
+        type="submit"
+      >
+        Acceder
+      </button>
+    </form>
   );
 }
 
