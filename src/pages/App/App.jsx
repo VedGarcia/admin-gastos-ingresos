63586ccfc7dc6616d4799937652f83217392f264
ZairBello <darwingamer00@gmail.com>
2025-04-24 12:22:36 -0400

    i fixed a bad import

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -1,12 +1,12 @@
 import { useRoutes, BrowserRouter } from "react-router-dom";
 import { GlobalProvider } from "../../context/GloblaState";
+import Login_registration from "../Login";
 import Incomes from "../incomes";
-import Login_registratio from "../Login";
 import NotFound from "../NotFound";
 
 const AppRoutes = () => {
   let routes = useRoutes([
-    { path: "/", element: <Login_registratio /> },
+    { path: "/", element: <Login_registration /> },
     { path: "/incomes", element: <Incomes /> },
     { path: "*", element: <NotFound /> },
   ]);
